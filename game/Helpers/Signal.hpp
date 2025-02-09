8265aa1d28d6babea2536d4291ff82dc429f089d
Zacharie Beauchemin <zacharie@beauchemin.dev>
2025-02-09 13:33:15 -0500

    Re implement expanded macros to cleanup Direction.hpp

--- game/Helpers/Signal.hpp
+++ game/Helpers/Signal.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#define CONNECT_SIGNAL(object, signalName, method)                  \
-if (object->connect(signalName, callable_mp(this, method)) != OK) { \
-  ERR_PRINT("Failed to connect " signalName " signal.");            \
+#define CONNECT_SIGNAL(object, signalName, method)                                                                    \
+if (object->connect(signalName, callable_mp(this, method)) != OK) {                                                   \
+  ERR_PRINT("Failed to connect " signalName " signal.");                                                              \
 }
--- game/Resources/Direction.hpp
+++ game/Resources/Direction.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../Helpers/GodotMacros.hpp"
+
 namespace Game {
 	enum class Direction {
 		Up,
@@ -22,78 +24,7 @@ namespace Game {
 }
 
 namespace godot {
-	// Expanded macro VARIANT_ENUM_CAST(Game::Direction) to support enum class
-	MAKE_ENUM_TYPE_INFO(Game::Direction)
+	VARIANT_ENUM_CLASS_CAST(Game::Direction)
 
-	template<>
-	struct VariantCaster<Game::Direction> {
-		static _FORCE_INLINE_ Game::Direction cast(const Variant& p_variant) {
-			return static_cast<Game::Direction>(p_variant.operator int64_t());
-		}
-	};
-
-	template<>
-	struct PtrToArg<Game::Direction> {
-		_FORCE_INLINE_ static Game::Direction convert(const void* p_ptr) {
-			return static_cast<Game::Direction>(*static_cast<const int64_t*>(p_ptr));
-		}
-
-		typedef int64_t EncodeT;
-		_FORCE_INLINE_ static void encode(Game::Direction p_val, void* p_ptr) {
-			*static_cast<int64_t*>(p_ptr) = static_cast<int64_t>(p_val);
-		}
-	};
-
-	// Expanded macro MAKE_TYPED_ARRAY(Game::Direction, godot::Variant::INT)
-	template<>
-	class TypedArray<Game::Direction> : public Array {
-	public:
-		_FORCE_INLINE_ void operator=(const Array& p_array) const {
-			ERR_FAIL_COND_MSG(!is_same_typed(p_array), "Cannot assign an array with a different element type.");
-			_ref(p_array);
-		}
-
-		_FORCE_INLINE_ TypedArray(const Variant& p_variant) : TypedArray(Array(p_variant)) {}
-		_FORCE_INLINE_ TypedArray(const Array& p_array) {
-			set_typed(Variant::INT, StringName(), Variant());
-			if (is_same_typed(p_array)) {
-				_ref(p_array);
-			} else {
-				assign(p_array);
-			}
-		}
-
-		_FORCE_INLINE_ TypedArray() {
-			set_typed(Variant::INT, StringName(), Variant());
-		}
-	};
-
-	// Expanded macro MAKE_TYPED_ARRAY_INFO(Game::Direction, godot::Variant::INT)
-	template<>
-	struct GetTypeInfo<TypedArray<Game::Direction>> {
-		static constexpr GDExtensionVariantType VARIANT_TYPE = GDEXTENSION_VARIANT_TYPE_ARRAY;
-		static constexpr GDExtensionClassMethodArgumentMetadata METADATA = GDEXTENSION_METHOD_ARGUMENT_METADATA_NONE;
-
-		static PropertyInfo get_class_info() {
-			return make_property_info(
-				Variant::Type::ARRAY,
-				"",
-				PROPERTY_HINT_ARRAY_TYPE,
-				Variant::get_type_name(Variant::INT).utf8().get_data());
-		}
-	};
-
-	template<>
-	struct GetTypeInfo<const TypedArray<Game::Direction>&> {
-		static constexpr GDExtensionVariantType VARIANT_TYPE = GDEXTENSION_VARIANT_TYPE_ARRAY;
-		static constexpr GDExtensionClassMethodArgumentMetadata METADATA = GDEXTENSION_METHOD_ARGUMENT_METADATA_NONE;
-
-		static PropertyInfo get_class_info() {
-			return make_property_info(
-				Variant::Type::ARRAY,
-				"",
-				PROPERTY_HINT_ARRAY_TYPE,
-				Variant::get_type_name(Variant::INT).utf8().get_data());
-		}
-	};
+	MAKE_TYPED_ARRAY_WITH_INFO(Game::Direction, godot::Variant::INT)
 }
