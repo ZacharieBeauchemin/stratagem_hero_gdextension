e4e23b669cddf5ee46f06f64c781709c9a1b7981
Zacharie Beauchemin <zacharie@beauchemin.dev>
2025-02-25 21:03:27 -0500

    Replace tabs with spaces

--- game/Helpers/GodotMacros.hpp
+++ game/Helpers/GodotMacros.hpp
@@ -1,76 +1,76 @@
 #pragma once
 
 // VARIANT_ENUM_CAST macro implementation to support enum class
-#define VARIANT_ENUM_CLASS_CAST(m_enum)                              																									\
-MAKE_ENUM_TYPE_INFO(m_enum)																																														\
-template<>																																																						\
-	struct VariantCaster<m_enum> {																																											\
-		static _FORCE_INLINE_ m_enum cast(const Variant& p_variant) {																											\
-			return static_cast<m_enum>(p_variant.operator int64_t());																												\
-		}																																																									\
-	};																																																									\
-																																																											\
-template<>																																																						\
-struct PtrToArg<m_enum> {																																															\
-	_FORCE_INLINE_ static m_enum convert(const void* p_ptr) {																														\
-		return static_cast<m_enum>(*static_cast<const int64_t*>(p_ptr));																									\
-	}																																																										\
-																																																											\
-	typedef int64_t EncodeT;																																														\
-	_FORCE_INLINE_ static void encode(m_enum p_val, void* p_ptr) {																											\
-		*static_cast<int64_t*>(p_ptr) = static_cast<int64_t>(p_val);																											\
-	}																																																										\
+#define VARIANT_ENUM_CLASS_CAST(m_enum)                                                                               \
+MAKE_ENUM_TYPE_INFO(m_enum)                                                                                           \
+template<>                                                                                                            \
+  struct VariantCaster<m_enum> {                                                                                      \
+    static _FORCE_INLINE_ m_enum cast(const Variant& p_variant) {                                                     \
+      return static_cast<m_enum>(p_variant.operator int64_t());                                                       \
+    }                                                                                                                 \
+  };                                                                                                                  \
+                                                                                                                      \
+template<>                                                                                                            \
+struct PtrToArg<m_enum> {                                                                                             \
+  _FORCE_INLINE_ static m_enum convert(const void* p_ptr) {                                                           \
+    return static_cast<m_enum>(*static_cast<const int64_t*>(p_ptr));                                                  \
+  }                                                                                                                   \
+                                                                                                                      \
+  typedef int64_t EncodeT;                                                                                            \
+  _FORCE_INLINE_ static void encode(m_enum p_val, void* p_ptr) {                                                      \
+    *static_cast<int64_t*>(p_ptr) = static_cast<int64_t>(p_val);                                                      \
+  }                                                                                                                   \
 };
 
 // Redefined and combined MAKE_TYPED_ARRAY and MAKE_TYPED_ARRAY_INFO macros
-#define MAKE_TYPED_ARRAY_WITH_INFO(m_type, m_variant_type)																														\
-template<>																																																						\
-class TypedArray<m_type> : public Array {																																							\
-public:																																																								\
-	_FORCE_INLINE_ void operator=(const Array& p_array) const {																													\
-		ERR_FAIL_COND_MSG(!is_same_typed(p_array), "Cannot assign an array with a different element type.");							\
-		_ref(p_array);																																																		\
-	}																																																										\
-																																																											\
-	_FORCE_INLINE_ TypedArray(const Variant& p_variant) : TypedArray(Array(p_variant)) {}																\
-	_FORCE_INLINE_ TypedArray(const Array& p_array) {																																		\
-		set_typed(m_variant_type, StringName(), Variant());																																\
-		if (is_same_typed(p_array)) {																																											\
-			_ref(p_array);																																																	\
-		} else {																																																					\
-			assign(p_array);																																																\
-		}																																																									\
-	}																																																										\
-																																																											\
-	_FORCE_INLINE_ TypedArray() {																																												\
-		set_typed(m_variant_type, StringName(), Variant());																																\
-	}																																																										\
-};																																																										\
-																																																											\
-template<>																																																						\
-struct GetTypeInfo<TypedArray<m_type>> {																																							\
-	static constexpr GDExtensionVariantType VARIANT_TYPE = GDEXTENSION_VARIANT_TYPE_ARRAY;															\
-	static constexpr GDExtensionClassMethodArgumentMetadata METADATA = GDEXTENSION_METHOD_ARGUMENT_METADATA_NONE;				\
-																																																											\
-	static PropertyInfo get_class_info() {																																							\
-		return make_property_info(																																												\
-			Variant::Type::ARRAY,																																														\
-			"",																																																							\
-			PROPERTY_HINT_ARRAY_TYPE,																																												\
-			Variant::get_type_name(m_variant_type).utf8().get_data());																											\
-	}																																																										\
-};																																																										\
-																																																											\
-template<>																																																						\
-struct GetTypeInfo<const TypedArray<m_type>&> {																																				\
-	static constexpr GDExtensionVariantType VARIANT_TYPE = GDEXTENSION_VARIANT_TYPE_ARRAY;															\
-	static constexpr GDExtensionClassMethodArgumentMetadata METADATA = GDEXTENSION_METHOD_ARGUMENT_METADATA_NONE;				\
-																																																											\
-	static PropertyInfo get_class_info() {																																							\
-		return make_property_info(																																												\
-			Variant::Type::ARRAY,																																														\
-			"",																																																							\
-			PROPERTY_HINT_ARRAY_TYPE,																																												\
-			Variant::get_type_name(m_variant_type).utf8().get_data());																											\
-	}\
+#define MAKE_TYPED_ARRAY_WITH_INFO(m_type, m_variant_type)                                                            \
+template<>                                                                                                            \
+class TypedArray<m_type> : public Array {                                                                             \
+public:                                                                                                               \
+  _FORCE_INLINE_ void operator=(const Array& p_array) const {                                                         \
+    ERR_FAIL_COND_MSG(!is_same_typed(p_array), "Cannot assign an array with a different element type.");              \
+    _ref(p_array);                                                                                                    \
+  }                                                                                                                   \
+                                                                                                                      \
+  _FORCE_INLINE_ TypedArray(const Variant& p_variant) : TypedArray(Array(p_variant)) {}                               \
+  _FORCE_INLINE_ TypedArray(const Array& p_array) {                                                                   \
+    set_typed(m_variant_type, StringName(), Variant());                                                               \
+    if (is_same_typed(p_array)) {                                                                                     \
+      _ref(p_array);                                                                                                  \
+    } else {                                                                                                          \
+      assign(p_array);                                                                                                \
+    }                                                                                                                 \
+  }                                                                                                                   \
+                                                                                                                      \
+  _FORCE_INLINE_ TypedArray() {                                                                                       \
+    set_typed(m_variant_type, StringName(), Variant());                                                               \
+  }                                                                                                                   \
+};                                                                                                                    \
+                                                                                                                      \
+template<>                                                                                                            \
+struct GetTypeInfo<TypedArray<m_type>> {                                                                              \
+  static constexpr GDExtensionVariantType VARIANT_TYPE = GDEXTENSION_VARIANT_TYPE_ARRAY;                              \
+  static constexpr GDExtensionClassMethodArgumentMetadata METADATA = GDEXTENSION_METHOD_ARGUMENT_METADATA_NONE;       \
+                                                                                                                      \
+  static PropertyInfo get_class_info() {                                                                              \
+    return make_property_info(                                                                                        \
+      Variant::Type::ARRAY,                                                                                           \
+      "",                                                                                                             \
+      PROPERTY_HINT_ARRAY_TYPE,                                                                                       \
+      Variant::get_type_name(m_variant_type).utf8().get_data());                                                      \
+  }                                                                                                                   \
+};                                                                                                                    \
+                                                                                                                      \
+template<>                                                                                                            \
+struct GetTypeInfo<const TypedArray<m_type>&> {                                                                       \
+  static constexpr GDExtensionVariantType VARIANT_TYPE = GDEXTENSION_VARIANT_TYPE_ARRAY;                              \
+  static constexpr GDExtensionClassMethodArgumentMetadata METADATA = GDEXTENSION_METHOD_ARGUMENT_METADATA_NONE;       \
+                                                                                                                      \
+  static PropertyInfo get_class_info() {                                                                              \
+    return make_property_info(                                                                                        \
+      Variant::Type::ARRAY,                                                                                           \
+      "",                                                                                                             \
+      PROPERTY_HINT_ARRAY_TYPE,                                                                                       \
+      Variant::get_type_name(m_variant_type).utf8().get_data());                                                      \
+  }                                                                                                                   \
 };
--- game/Resources/Direction.hpp
+++ game/Resources/Direction.hpp
@@ -3,28 +3,28 @@
 #include "../Helpers/GodotMacros.hpp"
 
 namespace Game {
-	enum class Direction {
-		Up,
-		Down,
-		Left,
-		Right
-	};
+  enum class Direction {
+    Up,
+    Down,
+    Left,
+    Right
+  };
 
-	constexpr const char* DirectionToString(const Direction direction) {
-		using enum Direction;
+  constexpr const char* DirectionToString(const Direction direction) {
+    using enum Direction;
 
-		switch (direction) {
-			case Up: return "Up";
-			case Down: return "Down";
-			case Left: return "Left";
-			case Right: return "Right";
-			default: return "Invalid";
-		}
-	}
+    switch (direction) {
+      case Up: return "Up";
+      case Down: return "Down";
+      case Left: return "Left";
+      case Right: return "Right";
+      default: return "Invalid";
+    }
+  }
 }
 
 namespace godot {
-	VARIANT_ENUM_CLASS_CAST(Game::Direction)
+  VARIANT_ENUM_CLASS_CAST(Game::Direction)
 
-	MAKE_TYPED_ARRAY_WITH_INFO(Game::Direction, godot::Variant::INT)
+  MAKE_TYPED_ARRAY_WITH_INFO(Game::Direction, godot::Variant::INT)
 }
